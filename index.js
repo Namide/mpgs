0f8d0f22de8b1c12351bd1f040c9c816e54e53f8
Namide <damien.git@gmail.com>
2016-02-05 16:54:58 +0100

    Load IP from server

--- index.js
+++ index.js
@@ -9,35 +9,37 @@
 
 
 
-console.log("			-------Check IP--------");
+console.log("- Check IP");
 var os = require('os');
 var ifaces = os.networkInterfaces();
+var IP = config.host;
 Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
 
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
+	var alias = 0;
+	ifaces[ifname].forEach(function (iface) {
 
-    if (alias >= 1) {
-		// this single interface has multiple ipv4 addresses
-		console.log(" ");
-		console.log("		" + ifname + ':' + alias, iface.address);
-    } else {
-		// this interface has only one ipv4 adress
-		console.log(" ");
-		console.log("		" + ifname, iface.address);
-    }
-    ++alias;
-  });
+		if (ifname == "eth0")
+			IP = iface.address;
+
+		if ('IPv4' !== iface.family || iface.internal !== false) {
+			// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+			return;
+		}
+
+		if (alias >= 1) {
+			// this single interface has multiple ipv4 addresses
+			console.log("	" + ifname + ':' + alias, iface.address);
+		} else {
+			// this interface has only one ipv4 adress
+			console.log("	" + ifname, iface.address);
+		}
+		++alias;
+	});
 });
-console.log(" ");
-console.log("			-----Start server-----");
 
 
 
+console.log("- Start server");
 
 // Init the lib WS
 var Server = require('./lib/Server');
@@ -48,13 +50,13 @@ var config = require('./config');
 
 // Init the server
 var server = new Server();
-var wss = new WebSocketServer({ port: process.env.port || config.port, host: (process.env.HOST || config.host), path: config.path });
+var wss = new WebSocketServer({ port: process.env.port || config.port, host: IP/*(process.env.HOST || config.host)*/, path: config.path });
 
-console.log(" ");
-console.log("		Server init: " + (process.env.HOST || config.host) + config.path + ":" + (process.env.port || config.port) );
-console.log(" ");
 
-console.log("			----------------------");
+console.log("	Server init: " + IP/*(process.env.HOST || config.host)*/ + config.path + ":" + (process.env.port || config.port) );
+
+
+
 
 
 // Listen the new connections
